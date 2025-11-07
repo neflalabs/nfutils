149a4588d9ff644bb128dea550acd1c9381b80d9
neflalabs <neflaprojekt@gmail.com>
2025-11-08 02:18:09 +0900

    simplified script

--- nfutils.sh
+++ nfutils.sh
@@ -14,7 +14,7 @@ ZSHRC="$HOME/.zshrc"
 ZSH_COMPLETION_DIR="$HOME/.zsh/completions"
 ZSH_COMPLETION_PATH="$ZSH_COMPLETION_DIR/_nfutils"
 NF_REPO_URL="https://raw.githubusercontent.com/neflalabs/nfutils/main/nfutils.sh"
-NF_VERSION="v2025-10-28T03:11:20-gdffae09"
+NF_VERSION="v2025-11-08T02:02:48-gac1fdaf"
 
 bold() { echo -e "\033[1m$1\033[0m"; }
 green() { echo -e "\033[32m$1\033[0m"; }
@@ -247,12 +247,11 @@ fi
 echo ""
 bold "🧰 Installing nfutils $NF_VERSION ..."
 mkdir -p "$NF_DIR"
-
 cat > "$NF_PATH" <<'EOF'
 #!/usr/bin/env bash
 set -e
 
-NF_VERSION="v2025-10-28T03:11:20-gdffae09"
+NF_VERSION="v2025-11-08T02:02:48-gac1fdaf"
 NF_REPO_URL="https://raw.githubusercontent.com/neflalabs/nfutils/main/nfutils.sh"
 
 BASHRC="$HOME/.bashrc"
@@ -747,7 +746,7 @@ case "$1" in
 esac
 EOF
 tmp_file=$(mktemp)
-sed "s|v2025-10-28T03:11:20-gdffae09|$NF_VERSION|g; s|https://raw.githubusercontent.com/neflalabs/nfutils/main/nfutils.sh|$NF_REPO_URL|g" "$NF_PATH" > "$tmp_file"
+sed "s|v2025-11-08T02:02:48-gac1fdaf|$NF_VERSION|g; s|https://raw.githubusercontent.com/neflalabs/nfutils/main/nfutils.sh|$NF_REPO_URL|g" "$NF_PATH" > "$tmp_file"
 mv "$tmp_file" "$NF_PATH"
 
 chmod +x "$NF_PATH"
@@ -881,6 +880,9 @@ _nfutils() {
                 ;;
               create)
                 _files -/
+                ;;
+              help)
+                _message 'show Laravel helper usage'
                 ;;
             esac
           fi
